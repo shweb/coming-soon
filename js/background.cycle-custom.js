658cf9bcc38a08f0a45918e2dad42b200f046fb7
hasiina <hasiina53@gmail.com>
2017-03-21 14:31:28 +0300

    template trustylabs

---
+++ js/background.cycle-custom.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+
+var fadeShow = $(".background").fadeShow({
+
+
+
+	correctRatio: true,
+
+
+
+	shuffle: true,
+
+
+
+	speed: 2500,
+
+
+
+	images: ['images/mer.jpg',
+
+	         'images/Baobab.jpg']
+
+
+
+
+
+
+
+});
